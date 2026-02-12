6455037c631fee1992d5e03fd3d735d951b7fbfe
skypper109 <colonel.diallo19@gmail.com>
2026-02-12 05:47:09 +0000

    Correction de l'erreur de cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:4200', 'https://admin.maboutique.tech', 'http://127.0.0.1:4200'],
+    'allowed_origins' => ['https://front.maboutique.tech', 'https://admin.maboutique.tech', 'http://localhost:4200', 'http://127.0.0.1:4200'],
 
     'allowed_origins_patterns' => [],
 
